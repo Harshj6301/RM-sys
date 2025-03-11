e63fab96396acc74a256769b99626ce0a1fca2b7
Harshvardhan Jadhav <jadhavharshvardhan6301@gmail.com>
2025-03-11 23:13:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,39 +54,44 @@ def main():
         sl_price = st.number_input("Stop Loss Price", min_value=0.0, value=95.0, step=0.05)
 
     with col3:
-        tgt_est = st.number_input("Estimated target", min_value=0.0, value=None, step=0.5)
+        tgt_est = st.number_input("Estimated target", min_value=0.0, value=200, step=0.5)
+        
     rpt = st.slider("Risk per Trade (%)", min_value=0.0, max_value=30.0, value=1.0, step=0.05)
 
     # calculation:
     slp = stop_loss_percent(entry_price, sl_price)
 
     if slp is not None: # only calculate position size if stop loss percent is a valid number.
-        psp, psc, psq = position_sizing(rpt, slp, capital, entry_price)
-
-        # Placeholder for analysis results (to be implemented later)
-        col1, col2 = st.columns(2)
-
-        with col1:
-            st.subheader("Optimal Position Sizing", divider='red')
-            st.write(f"Stop loss in Percent: {slp:.2f}%")
-            st.write(f"Position Size in Percent: {psp:.2f}%")
-            st.write(f"Position Size in Capital: {psc:.2f}")
-            st.write(f"Position Size in Quantity: {psq:.2f}")
-
-        with col2:
-            st.subheader("Per trade analysis", divider='red')
-            lot_size = st.number_input("Lot Size in Quantity", min_value=0, value=75, step=5)
-            no_of_lots = st.number_input("Number of Lots", min_value=1, value=1, step=1)
-            total_size = lot_size * no_of_lots 
-            st.write(f"Total Quantity: {total_size}")
-            buy_size = total_size * entry_price 
-            st.write(f"Total buy size: {buy_size}")
-            st.write(f"Total SL: {buy_size - (total_size * sl_price)}")
-            st.write(f"Profit range: {(tgt_est * total_size) - (buy_size)}")
-            st.write(f"R:R ratio: {calculate_rr_ratio(entry_price, sl_price, tgt_est)}")
-            
+        try:
+            psp, psc, psq = position_sizing(rpt, slp, capital, entry_price)
+    
+            # Placeholder for analysis results (to be implemented later)
+            col1, col2 = st.columns(2)
+    
+            with col1:
+                st.subheader("Optimal Position Sizing", divider='red')
+                st.write(f"Stop loss in Percent: {slp:.2f}%")
+                st.write(f"Position Size in Percent: {psp:.2f}%")
+                st.write(f"Position Size in Capital: {psc:.2f}")
+                st.write(f"Position Size in Quantity: {psq:.2f}")
+    
+            with col2:
+                st.subheader("Per trade analysis", divider='red')
+                lot_size = st.number_input("Lot Size in Quantity", min_value=0, value=75, step=5)
+                no_of_lots = st.number_input("Number of Lots", min_value=1, value=1, step=1)
+                total_size = lot_size * no_of_lots 
+                st.write(f"Total Quantity: {total_size}")
+                buy_size = total_size * entry_price 
+                st.write(f"Total buy size: {buy_size}")
+                st.write(f"Total SL: {buy_size - (total_size * sl_price)}")
+                st.write(f"Profit range: {(tgt_est * total_size) - (buy_size)}")
+                st.write(f"R:R ratio: {calculate_rr_ratio(entry_price, sl_price, tgt_est)}")
+        except:
+            pass
     else:
         st.write("Please check entry price and stop loss price.")
+
+    
 
 if __name__ == "__main__":
     main()
