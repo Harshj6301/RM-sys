785bad1e3cf49d19b37b5ae2a681970c95ec5bd5
Harshvardhan Jadhav <jadhavharshvardhan6301@gmail.com>
2025-03-12 14:36:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,8 +105,13 @@ def main():
                 "R:R Ratio": [rr_ratio]
                 }
             df = pd.DataFrame(data)
-            st.subheader('Graphs', divider='red') 
-            st.bar_chart(np.linspace(0,100,1), df[['R:R Ratio']], horizontal=True, stack=False)
+            st.subheader('Graphs', divider='red')
+            data1 = {
+                "ratios":[np.linspace(0,100,1)],
+                "R:R Ratio":[rr_ratio]
+                    }
+            df1 = pd.DataFrame(data1)
+            st.bar_chart(df1[['R:R Ratio']], horizontal=True, stack=False)
             col1, col2 = st.columns(2)
             with col1:
                 st.bar_chart(df[['Buy Size', 'Stop Loss Amount', 'Profit Range']], x_label='Total amount', stack=False)
