64405d893e72cdb95fbc90925c3ddd7b2c6a4302
Harshvardhan Jadhav <jadhavharshvardhan6301@gmail.com>
2025-03-14 22:05:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def main():
                 st.plotly_chart(fig_pie, theme="streamlit", use_container_width=True)
             with col2:
                # st.bar_chart(df[['Entry Price', 'Stop Loss Price', 'Target Price']], x_label='Trade levels', stack=False, horizontal=True)
-                fig_bar = px.bar(df, y=['Entry Price', 'Stop Loss Price', 'Target Price'], title='Trade Levels', template='plotly_dark', color_discrete_sequence=['darkorange'], orientation='h')
+                fig_bar = px.bar(df, y=['Entry Price', 'Stop Loss Price', 'Target Price'], title='Trade Levels', template='plotly_dark', color_discrete_sequence=['darkorange'])
                 st.plotly_chart(fig_bar, theme=None, use_container_width=True)
 
         
